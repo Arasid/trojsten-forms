f079e1f4e313d7d9d72397f88d67dfb1e7970da9
Barbora Klembarova <basa.akrobat@gmail.com>
2016-04-14 13:10:06 +0200

    indentacia

--- js/webpack.conf.js
+++ js/webpack.conf.js
@@ -13,14 +13,18 @@ const base = {
     },
     module: {
         loaders: [
-        {
-            test : /\.js$/,
-            include : path.join(__dirname, 'src'),
-            loader : 'babel-loader',
-            query : {
-                presets : ['es2015', 'react', 'stage-0']
-            }
-        },
+            {
+                test : /\.js$/,
+                include : path.join(__dirname, 'src'),
+                loader : 'babel-loader',
+                query : {
+                    presets : ['es2015', 'react', 'stage-0']
+                }
+            },
+            {
+                test : /\.css$/,
+                loader: 'style-loader!css-loader'
+            },
         ],
     },
 }
